38509bc51dc7791c2f862d66230d66a76e8c4943
Nico Andrade <nicolas.andrade@ssense.com>
2018-12-28 17:47:52 -0500

    Maze

--- scripts/maze/index.js
+++ scripts/maze/index.js
@@ -11,9 +11,27 @@ request.get('https://vents.kringlecastle.com/', function(err, res) {
     const dom = new JSDOM(sourceCode);
     const $ = (require('jquery'))(dom.window);
 
-    const script = $('script');
-    console.log(script['0'], script['1'], script['2']);
+    const jsLines = $('script')[2].text.split("\n");
+    // console.log(jsLines);
 
+    const interestingValues = ['mazef', 'mazex','mazey','locationkey'];
+
+    const mapPosition = {}
+    interestingValues.forEach((interestingValue) => {
+        jsLines.forEach((jsLine) => {
+
+            const worth = jsLine.includes('"' + interestingValue + '"');
+            if(!worth){
+                return ;
+            }
+            console.log("Line ", jsLine, "contains the interesting value", interestingValue);
+            const foundValue = jsLine.match(/(.*).value = "(.*)"/)[2];
+            mapPosition[interestingValue] = foundValue;
+
+        })
+    })
+
+    console.log(mapPosition);
 
 
 })
